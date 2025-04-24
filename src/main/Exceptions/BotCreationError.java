068e4088ce752c8d39390073fded3ebfadf0da3e
mattiias00 <matheusmatts44@gmail.com>
2025-04-24 11:25:30 -0300

    Creating and adding DataBase

--- src/main/Exceptions/BotCreationError.java
+++ src/main/Exceptions/BotCreationError.java
@@ -1,7 +1,4 @@
 package src.main.Exceptions;
-
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-
 public class BotCreationError extends RuntimeException {
   public BotCreationError(String message) {
     super(message);
