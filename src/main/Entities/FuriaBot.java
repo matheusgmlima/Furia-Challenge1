1e0e279a0e3849da790ceb15a2f3c5525475a930
mattiias00 <matheusmatts44@gmail.com>
2025-04-24 23:55:43 -0300

    Really latest changes

--- src/main/Entities/FuriaBot.java
+++ src/main/Entities/FuriaBot.java
@@ -19,13 +19,9 @@ public class FuriaBot extends TelegramLongPollingBot {
 
     @Override
     public String getBotUsername() {
-        return "@challengefuria_bot"; // Substitua com @seu_bot
+        return "@challengefuria_bot";
     }
 
-    @Override
-    public String getBotToken() {
-        return "7502628820:AAEjKUyrBMB4Kki1E1KVPDQJ6sdLz-nMlRQ"; // Sua API Token
-    }
 
     @Override
     public void onUpdateReceived(Update update) {
